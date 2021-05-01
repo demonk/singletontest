a1079b6a3592efe6ad915c3c8c69de765c812f9b
guosen.lgs <guosen.lgs@alibaba-inc.com>
2021-05-01 22:31:03 +0800

    Initial Commit

---
+++ SingletonTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  SingletonTest
+//
+//  Created by SamsenLee on 2021/5/1.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
